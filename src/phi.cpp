3d8a26ca44fc83925e5dfbe9302280b949662c82
Matthew A. Wolff <wolff1@purdue.edu>
2014-08-11 20:25:05 -0400

    Fixed sum of fine grids, wrote code for command file.
    The fine grid values needed to be multiplied by the coefficients which
    were calculated in a previous commit. This fixed the sum of the fine grids
    matching the coarse grid.
    
    I played with different gnuplot options in the command file. It seems that
    dashed lines on the windows terminal is not easy?

--- src/phi.cpp
+++ src/phi.cpp
@@ -202,28 +202,39 @@ void phi_nesting_test(void)
 	double		ff = 0.0;	// Function value on fine grid
 	double		fc = 0.0;	// Function value on course grid
 	double		f = 0.0;	// Sum of fine grid contributions
+	double		maxf = 0.0;
 	double		tol = pow(0.1,15);
     char*		buf2_1 =    "set term %s\n"
+							"set termoption dash\n"
 		                    "set xlabel 'u'\n"
 //			                "set ylabel 'g_l(r)'\n" // below two lines rotates by -90 degrees
 							"set lmargin 10\n"
-							"set label 1 'Phi(u)' at graph -0.1, graph 0.5\n"
+							"set label 1 'f(u)' at graph -0.1, graph 0.5\n"
 							"set title 'Nesting of Spline Function Spaces'\n"
 							"set grid\n"
-//	                      "set style data lines\n"
-							"set style data linespoints\n"
+							"set style line 1 lc rgb \"cyan\"\n"
+							"set style line 2 lc rgb \"blue\"\n"
+							"set style line 3 lc rgb \"red\"\n"
+//							"set style data lines\n"
+//							"set style data linespoints\n"
 							"set yrange [ 0.0 : %7.3f ]\n"
 							"plot ";
-    char*		buf2_2 =    "data_file using 1:%d title \"%s\" lc rgb \"black\",";
-    char*		buf2_3 =    "data_file using 1:%d title \"%s\" lc rgb \"black\"\n"
+    //char*		buf2_2 =    "data_file using 1:%d title \"Phi(2u%+d)\" lc rgb \"black\",";
+    //char*		buf2_3 =    "data_file using 1:%d title \"Sum of fine grids\" lc rgb \"black\","
+				//			"data_file using 1:%d title \"Phi(u)\" lc rgb \"black\"\n"
+				//			"pause -1\n"
+				//			"quit\n";
+    char*		buf2_2 =    "data_file using 1:%d with lines title \"Phi(2u%+d)\" ls 1,";
+    char*		buf2_3 =    "data_file using 1:%d with lines title \"Sum of fine grids\" ls 2,"
+							"data_file using 1:%d with lines title \"Phi(u)\" ls 3\n"
 							"pause -1\n"
 							"quit\n";
-
 	// Dynamically allocated memory variables
 	FILE*		fp = NULL;
 	char*		data_file = NULL;
 	char*		cmd_file = NULL;
 	char*		buf = NULL;
+	double*		g2fg = NULL;
 
 	/**************************************************************************/
 	// Get number of data points to record
@@ -239,8 +250,13 @@ void phi_nesting_test(void)
 	// Create arrays for dependent and independent variables
 	data_file = (char*) dynvec(GP_DATA_DIR_LEN + GP_DATA_TMP_LEN + 1,sizeof(char));
 	cmd_file = (char*) dynvec(GP_CMD_DIR_LEN + cmd_file_name_len + 1,sizeof(char));
+	g2fg = (double*) dynvec(p/2+1,sizeof(double));
 	assert(data_file != NULL);
 	assert(cmd_file != NULL);
+	assert(g2fg != NULL);
+
+	// Calculate the coefficients to display
+	compute_g2fg(p, g2fg);
 
 	// Build file name(s)
 	sprintf(data_file, "%s%s", GP_DATA_DIR, GP_DATA_TMP);
@@ -250,7 +266,7 @@ void phi_nesting_test(void)
 	fp = fopen(data_file, "w");
 	assert(fp != NULL);
 
-	bufmax = 64*(p+3);	// p+3 columns, each a max of 64 chars wide 
+	bufmax = 64*(p+4);	// p+4 columns, each a max of 64 chars wide 
 	buf = (char*) dynvec(bufmax+1,sizeof(char));	// + 1 for NULL
 
 	/**** WRITE THE DATA FILE WHILE CALCULATING ALL THE REQUIRED VALUES*******/
@@ -261,30 +277,47 @@ void phi_nesting_test(void)
 
 		buflen = sprintf(buf, "%.32f", x);
 		assert(bufmax > buflen);
+f = 0.0;
 
 		//	Columns 2 to p+2 (fine grid interpolant values)
-		ff = phi(p, 2.0*x, NULL);
-		f = ff;
+		for (j = 1; j <= p/2; j++)
+		{
+			ff = g2fg[j]*phi(p, 2.0*x-j, NULL);
+f += ff;
+			buflen += sprintf(&buf[buflen], " %.32f", ff);
+			assert(bufmax > buflen);
+		}
+
+		ff = g2fg[0]*phi(p, 2.0*x, NULL);
+f += ff;
 		buflen += sprintf(&buf[buflen], " %.32f", ff);
 		assert(bufmax > buflen);
 
 		for (j = 1; j <= p/2; j++)
 		{
-			ff = phi(p, 2.0*x+j, NULL);
-			f += ff;
-			buflen += sprintf(&buf[buflen], " %.32f", ff);
-			assert(bufmax > buflen);
-
-			ff = phi(p, 2.0*x-j, NULL);
-			f += ff;
+			ff = g2fg[j]*phi(p, 2.0*x+j, NULL);
+f += ff;
 			buflen += sprintf(&buf[buflen], " %.32f", ff);
 			assert(bufmax > buflen);
 		}
 
-		// Column p+3 (course grid interpolant values)
+		// Column p+3 (sum of fine grid interpolant values)
+		buflen += sprintf(&buf[buflen], " %.32f", f);
+		assert(bufmax > buflen);
+
+		// Column p+4 (course grid interpolant values)
 		fc = phi(p, x, NULL);
 		buflen += sprintf(&buf[buflen], " %.32f\n", fc);
 		assert(bufmax > buflen);
+		if (fc > maxf)
+		{
+			maxf = fc;
+		}
+if (fabs(f - fc) >= tol)
+{
+	printf("|f-fc| = %e\n", fabs(f-fc));
+}
+assert(fabs(f - fc) < tol);
 
 		// Write buffer to file
 		bytes = fwrite(buf, sizeof(char), buflen, fp);
@@ -296,9 +329,6 @@ void phi_nesting_test(void)
 
 		// Clear out buffer for next usage
 		memset(buf, 0, bufmax+1);
-
-printf("error = %e\n", fabs(f - fc));
-		assert(fabs(f - fc) < tol);
 	}
 
 	// Close DATA file (ensure buffer is flushed to disk)
@@ -309,21 +339,41 @@ printf("error = %e\n", fabs(f - fc));
 
 	// Free buffer and re-allocate it for next file
 	dynfree(buf);
-	bufmax = 1024;
+	bufmax = strlen(buf2_1) + (p+2)*(strlen(buf2_2)+10) + strlen(buf2_3) + 64;
 	buf = (char*) dynvec(bufmax+1,sizeof(char));
 
-	// Create COMMAND file
+	/*****WRITE THE COMMAND FILE TO DISPLAY THE RESULTS OF ABOVE *************/
 	fp = fopen(cmd_file, "w");
 	assert(fp != NULL);
 
-	//	...
+	// Create COMMAND file buffer
+	buflen = sprintf(buf, buf2_1, GP_TERM, 1.25*maxf);
+	assert(bufmax > buflen);
+
+	// Plot a line for each fine grid b-spline (p+1 fine grids, 1 sum of fine grids)
+	for (i = 0; i < p+1; i++)
+	{
+		buflen += sprintf(&buf[buflen], buf2_2, i+2, i-p/2);
+		assert(bufmax > buflen);
+	}
+
+	// Plot line for 1/r (column nlev+2) and finish file
+	buflen += sprintf(&buf[buflen], buf2_3, p+3, p+4);
+	assert(bufmax > buflen);
+
+	// Write buffer to file
+	bytes = fwrite(buf, sizeof(char), buflen, fp);
+	if (bytes < buflen)
+	{
+		printf("<%lu> bytes written to temporary file <%s> instead of <%lu>\n", bytes, cmd_file, buflen);
+	}
 
 	// Close COMMAND file (ensure buffer is flushed to disk)
 	if (fclose(fp))
 	{
 		printf("Error closing COMMAND file.\n");
 	}
-/*
+
 	// Call gnuplot to plot DATA file using COMMAND file
 	plotf2d(cmd_file, data_file);
 
@@ -338,12 +388,13 @@ printf("error = %e\n", fabs(f - fc));
 	{
 		printf("Error removing COMMAND file <%s>.\n", cmd_file);
 	}
-*/
+
 	/**************************************************************************/
 	// Free allocated memory
 	dynfree(data_file);
 	dynfree(cmd_file);
 	dynfree(buf);
+	dynfree(g2fg);
 }
 
 //	End of file
