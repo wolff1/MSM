73c96df051a9d5c173be3759175608e49d295127
Matthew A. Wolff <wolff1@purdue.edu>
2015-01-30 17:22:51 -0500

    parallel allocation test code in main.c.

--- src/main.c
+++ src/main.c
@@ -274,12 +274,13 @@ void test_parallel_division(void)
 	long		N = 0;				//	# particles
 	long		K = 0;				//	# grid points
 	long		P = 0;				//	# procs
-	double		Ps = 0.0;			//	# procs split 1
-	double		Pt = 0.0;			//	# procs split 2
 	double		a = 0.0;			//	cut-off distance
 	double		h = 0.0;			//	grid spacing
-	double		alpha = 0.0;		//	Half stencil radius
 	short		p = 0;				//	Order of interpolant
+
+	double		alpha = 0.0;		//	Half stencil radius
+	double		Ps = 0.0;			//	# procs split 1
+	double		Pt = 0.0;			//	# procs split 2
 	long		D0 = 0;
 	long		D1 = 0;
 	long		D2 = 0;
@@ -292,50 +293,116 @@ void test_parallel_division(void)
 
 	printf("Please enter particles N: ");
 	scanf("%ld", &N);
-//	printf("Please enter grid points K: ");
-//	scanf("%ld", &K);
-	K = N;
+	printf("Please enter grid points K: ");
+	scanf("%ld", &K);
 	printf("Please enter processors P: ");
 	scanf("%ld", &P);
-	P = MAX(P,100);
 	printf("Please enter cut-off a: ");
 	scanf("%lf", &a);
 	printf("Please enter grid spacing h: ");
 	scanf("%lf", &h);
-	alpha = 2.0*a/h;
 	printf("Please enter order p: ");
 	scanf("%hd", &p);
+
+	alpha = 2.0*a/h;
 	printf("2*alpha = %f\n", alpha);
 
-//	printf("You entered: N=%ld, K=%ld, P=%ld\n", N, K, P);
+/*
+	Work per step = (Work per point)*(number of points)
+	Time per step = (Time/Work)*Work
+		=> 1 processor can do 1 unit Work in 1 unit time
+		=> We're not trying to balance work, per se, we're trying to balance *time*
 
-	//	4-level method
-	D0 = (long)ceil(pow(alpha,3))*K;
-	D1 = (long)ceil(pow(alpha,3))*(long)ceil((double)K/8.0);
-	D2 = (long)ceil((double)K*K/pow(64.0,2));
-	R1 = p*p*p*(long)ceil(K/pow(8.0,1));
-	R2 = p*p*p*(long)ceil(K/pow(8.0,2));
-	P1 = R2;
-	P0 = R1;
-	printf("Here are the complexities:\nD0=%ld\nR1=%ld, D1=%ld, P0=%ld\nR2=%ld, D2=%ld, P1=%ld\n", D0, R1, D1, P0, R2, D2, P1);
+	D0 ~= R1 + D1 + P0:
+	[(32*pi/3)*(alpha)^3]*K ~= [(p+1)^3]*K + [(4*pi/3)*(alpha)^3]*K + [(p+1)^3]*K
+	[(32*pi/3)*(alpha)^3]    = [(p+1)^3] +   [(4*pi/3)*(alpha)^3] +   [(p+1)^3]
 
-	srand(time(NULL));
-	Kconst = (double)rand()/((double)RAND_MAX);
-	Kconst = 0.05 + (0.20)*Kconst;
-	Kfake = (double)D0 + Kconst*D0;
-//	printf("Kfake = %f, Kconst=%f\n", Kfake, Kconst);
-//	printf("D0/Kfake = %f\n", (double)D0/Kfake);
-	Ps = (double)P - (double)P*(double)D0/Kfake;		//	D0/K should be close to 1 so that Ps is relatively small
-	Pt = Ps*(R2+D2+P1)/(double)(D1+R2+D2+P1);	//	Pt will be even less than Ps
-//	printf("P = %ld, s = %f, t = %f, Ps = %ld, Pt = %ld\n", P, Ps, Pt, (long)ceil(Ps), (long)ceil(Pt));
-//	printf("\n\n");
-	Ps = ceil(Ps);
-	Pt = ceil(Pt);
-	printf("P = %ld, P-s = %ld, s = %ld, s-t = %ld, t = %ld\n", P, P-(long)Ps, (long)Ps, (long)Ps-(long)Pt, (long)Pt);
+	T(D0,P-S) = T(R1,S) + T(D1,S-T) + T(P0, S):
+	[(32*pi/3)*(alpha)^3]/(P-S)  =   [(p+1)^3]/S +   [(4*pi/3)*(alpha)^3]/(S-T) +   [(p+1)^3]/S
+	[(32*pi/3)*(alpha)^3]/(P-S)  = 2*[(p+1)^3]/S +   [(4*pi/3)*(alpha)^3]/(S-T)
 
-	printf("#1: D0 / (P-s) = K / P, %f ?= %f\n", (double)D0/((double)P-Ps), Kfake / (double)P);
-	printf("#2: D0 / (P-s) = R1/s + D1/(s-t) + P0/s => %f ?= %f\n", (double)D0/((double)P-Ps), (double)R1/Ps + (double)D1/(Ps-Pt) + (double)P0/Ps);
-	printf("#3: D1 / (s-t) = R2/t + D2/t + P1/t => %f ?= %f\n", (double)D1/(Ps-Pt), (double)(R2 + D2 + P1)/Pt);
+	//	D{0} to D{L-2}
+	T(D{i},P{i}) = T(R{i+1},P{i-1}-P{i}) + T(D{i+1},P{i}-P{i+1}) + T(Pr{0}, P{i}):
+	[(32*pi/3)*(alpha)^3]/(P{i})  = 2*[(p+1)^3]/(P{i-1}-P{i}) +   [(4*pi/3)*(alpha)^3]/(P{i}-P{i+1}); 0 <= i <= L-3; P{-1} = P; 
+
+	D{i}   = (32*pi/3)*(alpha)^3;
+	R{i+1} = (p+1)^3;
+	D{i+1} = (4*pi/3)*(alpha)^3;	//	-> (1/8)*D{i}
+	Pr{i}  = (p+1)^3;
+	D{top} = 8^{-(L-2)}K
+
+	P{i}/((32*pi/3)*(alpha)^3)  = (P{i-1}-P{i})/(2*(p+1)^3) +   (P{i}-P{i+1})/((4*pi/3)*(alpha)^3)
+	P{i}/(D{i})  = (P{i-1}-P{i})/(R{i+1}+Pr{i}) +   (P{i}-P{i+1})/(D{i+1})
+*/
 }
+
+//void test_parallel_division(void)
+//{
+//	long		N = 0;				//	# particles
+//	long		K = 0;				//	# grid points
+//	long		P = 0;				//	# procs
+//	double		Ps = 0.0;			//	# procs split 1
+//	double		Pt = 0.0;			//	# procs split 2
+//	double		a = 0.0;			//	cut-off distance
+//	double		h = 0.0;			//	grid spacing
+//	double		alpha = 0.0;		//	Half stencil radius
+//	short		p = 0;				//	Order of interpolant
+//	long		D0 = 0;
+//	long		D1 = 0;
+//	long		D2 = 0;
+//	long		R1 = 0;
+//	long		R2 = 0;
+//	long		P1 = 0;
+//	long		P0 = 0;
+//	double		Kfake = 0.0;
+//	double		Kconst = 0.0;
+//
+//	printf("Please enter particles N: ");
+//	scanf("%ld", &N);
+////	printf("Please enter grid points K: ");
+////	scanf("%ld", &K);
+//	K = N;
+//	printf("Please enter processors P: ");
+//	scanf("%ld", &P);
+//	P = MAX(P,100);
+//	printf("Please enter cut-off a: ");
+//	scanf("%lf", &a);
+//	printf("Please enter grid spacing h: ");
+//	scanf("%lf", &h);
+//	alpha = 2.0*a/h;
+//	printf("Please enter order p: ");
+//	scanf("%hd", &p);
+//	printf("2*alpha = %f\n", alpha);
+//
+////	printf("You entered: N=%ld, K=%ld, P=%ld\n", N, K, P);
+//
+//	//	4-level method
+//	D0 = (long)ceil(pow(alpha,3))*K;
+//	D1 = (long)ceil(pow(alpha,3))*(long)ceil((double)K/8.0);
+//	D2 = (long)ceil((double)K*K/pow(64.0,2));
+//	R1 = p*p*p*(long)ceil(K/pow(8.0,1));
+//	R2 = p*p*p*(long)ceil(K/pow(8.0,2));
+//	P1 = R2;
+//	P0 = R1;
+//	printf("Here are the complexities:\nD0=%ld\nR1=%ld, D1=%ld, P0=%ld\nR2=%ld, D2=%ld, P1=%ld\n", D0, R1, D1, P0, R2, D2, P1);
+//
+//	srand(time(NULL));
+//	Kconst = (double)rand()/((double)RAND_MAX);
+//	Kconst = 0.05 + (0.20)*Kconst;
+//	Kfake = (double)D0 + Kconst*D0;
+////	printf("Kfake = %f, Kconst=%f\n", Kfake, Kconst);
+////	printf("D0/Kfake = %f\n", (double)D0/Kfake);
+//	Ps = (double)P - (double)P*(double)D0/Kfake;		//	D0/K should be close to 1 so that Ps is relatively small
+//	Pt = Ps*(R2+D2+P1)/(double)(D1+R2+D2+P1);	//	Pt will be even less than Ps
+////	printf("P = %ld, s = %f, t = %f, Ps = %ld, Pt = %ld\n", P, Ps, Pt, (long)ceil(Ps), (long)ceil(Pt));
+////	printf("\n\n");
+//	Ps = ceil(Ps);
+//	Pt = ceil(Pt);
+//	printf("P = %ld, P-s = %ld, s = %ld, s-t = %ld, t = %ld\n", P, P-(long)Ps, (long)Ps, (long)Ps-(long)Pt, (long)Pt);
+//
+//	printf("#1: D0 / (P-s) = K / P, %f ?= %f\n", (double)D0/((double)P-Ps), Kfake / (double)P);
+//	printf("#2: D0 / (P-s) = R1/s + D1/(s-t) + P0/s => %f ?= %f\n", (double)D0/((double)P-Ps), (double)R1/Ps + (double)D1/(Ps-Pt) + (double)P0/Ps);
+//	printf("#3: D1 / (s-t) = R2/t + D2/t + P1/t => %f ?= %f\n", (double)D1/(Ps-Pt), ((double)(R2 + D2 + P1))/Pt);
+//}
 
 // End of file
